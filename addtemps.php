2a215c85eb5c4856c75c26a1af6ef7c29aa8f5c6
Cory Cloutier <ccloutier@wisc.edu>
2015-08-23 16:56:02 -0500

    Fixed addtemps.php to match the changes to temps.php

--- addtemps.php
+++ addtemps.php
@@ -7,59 +7,94 @@ DEFINE("ADMINLEVELNEEDED",'AddTemp');
 include_once("include_files/access.inc.php");
 $db = new mysqli($db_server, $db_username, $db_password, $db_name);
 
+$temps = FALSE;
+$station = FALSE;
+$user = FALSE;
+$temps = FALSE;
+$unit = FALSE;
+$inserts = array();
+
 $station = Is_Set_Request('Station',$db);
-$item = Is_Set_Request('Item',$db);
-$temp = Is_Set_Request('Temp',$db);
-$unit = Is_Set_Request('Unit',$db);
 $user = $_SESSION['USER'];
-$isOutOfRange = 0;
 
-/* Check to see if the temp is out of range */ 
-$query = "SELECT `Temp` FROM `ITEMS` WHERE `id` = '$item'";
-$results = $db->query($query);
-$result = $results->fetch_assoc();
-$expectedTemp = $result['Temp'];
-switch ($expectedTemp) {
-	case 'Hot':
-		if ($temp < 135.0) {
-			echo "<div>Warning! Temperature is too cold.</div>\n<a href='temps.php'>Return to Temps</a>";
-			$isOutOfRange = 1;
-		}
-	break;
-	case 'Cold':
-		if ($temp > 41.0) {
-			echo "<div>Warning! Temperature is too warm.</div>\n<a href='temps.php'>Return to Temps</a>";
-			$isOutOfRange = 1;
-		}
-	break;
-	case 'Frozen':
-		if ($temp > 25.0) {
-			echo "<div>Warning! Temperature is too warm.</div>\n<a href='temps.php'>Return to Temps</a>";
-			$isOutOfRange = 1;
-		}
-	break;
-	default:
-		echo "ERROR: AddTemp Expected Temp error: $expectedTemp";
+if(isset($_REQUEST['temp']) && !empty($_REQUEST['temp'])) {
+	$temps = $_REQUEST['temp'];
 }
 
-/* Insert new temp into the temp table then take any necessary action */
-$sql = "INSERT INTO `TEMPS` (`Item`, `Temp`,`Unit`,`Station`,`Time`,`EnteredBy`,`IsOutOfRange`) VALUES ($item,$temp,$unit,$station,now(),$user,$isOutOfRange)";
-if ($db->query($sql) === TRUE) {
-	if ($isOutOfRange === 1){
-		/* If temp not in normal range email management */
-		//Add email code here
-		exit();
-	} else {
-		/* If temp in normal range redirect back to temps.php */
-		$db->close();
-		$host  = $_SERVER['HTTP_HOST'];
-		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra = 'temps.php';  // change accordingly
-		header("Location: http://$host$uri/$extra");
-		exit;
-	}
+if(Check_Admin_String('ChangeUnit',$_SESSION['ADMINLEVEL'])){ 
+	$unit = Is_Set_Request('Unit',$db);
 } else {
-	echo "Error: $sql AddTemp - " . $db->error ;
-	$db->close();
+	$unit = $_SESSION['UNIT'];
 }
+
+if($station != FALSE && $user != FALSE && $unit != FALSE && $temps != FALSE){
+	foreach($temps as $item => $temp) {
+		if(is_numeric($item)) {
+			$item = $db->real_escape_string($item);
+		} else {
+			$item = FALSE;
+		}
+		if(is_numeric($temp)) {
+			$temp = $db->real_escape_string($temp);
+		} else {
+			$temp = FALSE;
+		}
+		if($temp !== FALSE && $item !== FALSE) {
+			/* Check to see if the temp is out of range */ 
+			$isOutOfRange = 0;
+			$query = "SELECT `Temp` FROM `ITEMS` WHERE `id` = '$item'";
+			$results = $db->query($query);
+			$result = $results->fetch_assoc();
+			$expectedTemp = $result['Temp'];
+			switch ($expectedTemp) {
+				case 'Hot':
+					if ($temp < 135.0) {
+						//echo "<div>Warning! Temperature is too cold.</div>\n<a href='temps.php'>Return to Temps</a>";
+						$isOutOfRange = 1;
+					}
+				break;
+				case 'Cold':
+					if ($temp > 41.0) {
+						//echo "<div>Warning! Temperature is too warm.</div>\n<a href='temps.php'>Return to Temps</a>";
+						$isOutOfRange = 1;
+					}
+				break;
+				case 'Frozen':
+					if ($temp > 25.0) {
+						//echo "<div>Warning! Temperature is too warm.</div>\n<a href='temps.php'>Return to Temps</a>";
+						$isOutOfRange = 1;
+					}
+				break;
+				default:
+					echo "ERROR: AddTemp Expected Temp error: $expectedTemp";
+			}
+			/* Insert new temp into the temp table then take any necessary action */
+			$sql = "INSERT INTO `TEMPS` (`Item`, `Temp`,`Unit`,`Station`,`Time`,`EnteredBy`,`IsOutOfRange`) VALUES ($item,$temp,$unit,$station,now(),$user,$isOutOfRange)";
+			
+			if ($db->query($sql) === TRUE) {
+				if ($isOutOfRange === 1){
+					$insertId = $db->insert_id;
+					$inserts[] = $insertId;
+				}
+			} else {
+				echo "Error: $sql AddTemp - " . $db->error ;
+			}
+		}
+	}
+}
+$db->close();
+
+if(count($inserts)==0) {
+	$host  = $_SERVER['HTTP_HOST'];
+	$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$extra = 'temps.php';  // change accordingly
+	header("Location: http://$host$uri/$extra");
+} else {
+	$_SESSION['OORTemps'] = $inserts;
+	$host  = $_SERVER['HTTP_HOST'];
+	$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$extra = 'correctiveaction.php';  // change accordingly
+	header("Location: http://$host$uri/$extra");
+}
+
 ?>
---
+++ correctiveaction.php
@@ -0,0 +1,8 @@
+<?php
+DEFINE("ADMINLEVELNEEDED",'AddTemp');
+include_once("include_files/access.inc.php");
+print_r($_SESSION['OORTemps']);
+
+
+
+?>
